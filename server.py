e51b813444978ed5af1b801cefdbec561596364e
Jon Friend <62109917+jonnyfriendlychicago@users.noreply.github.com>
2022-03-01 14:58:02 -0600

    xyz

--- server.py
+++ server.py
@@ -5,32 +5,37 @@ app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 
 
 @app.route('/')          # The "@" decorator associates this route with the function immediately following
-def hello_world():
-    # return 'Hello World (playground)'  # Return the string 'Hello World!' as a response
-    return render_template("index.html", phrase="whatTheWhat", times=100)
+def welcome():
+    return render_template("index.html", displayThisMany = 0, displayThisColorYknow = "aqua") 
 
-# @app.route('/dojo')
-# def dojo(): 
-#     return "You're in the Dojo, baby!"
+@app.route('/play')
+def play():
+    return render_template("index.html", displayThisMany=70, displayThisColorYknow = "aqua")
 
-# # Create a route that responds with "Hi" and whatever name is in the URL after /say/
-# @app.route('/say/<string:elNameo>')
-# def hello(elNameo):
-#     return f"Hiya, {elNameo}."
+@app.route('/play/<int:num>')
+def playNumber(num):
+    return render_template("index.html", displayThisMany=num, displayThisColorYknow = "aqua")
 
-# # Create a route that responds with the given word repeated as many times as specified in the URL
+@app.route('/play/<int:num>/<string:incomingColor>')
+def playNumberColor(num, incomingColor):
+    if incomingColor == "blue": 
+        selectedColor = "blue" 
+    elif incomingColor == "green": 
+        selectedColor = "green"
+    else: 
+        selectedColor = "red"
+    # selectedColor = incomingColor 
+    return render_template("index.html", displayThisMany=num, displayThisColorYknow = selectedColor )
 
-# @app.route('/repeat/<int:multiplier>/<string:yourWord>')
-# def greetaBunch(multiplier,yourWord ):
-#     return f"{yourWord * multiplier}."
 
-
-"""below always needs to be at the bottom of the script, yes!"""
+"""DON'T TOUCH BELOW :-) below always needs to be at the bottom of the script, yes!"""
 # below is stuff you oughta have, per cameron smith: 
+
 @app.route('/', defaults={'cookies': ''})
 @app.route('/<path:cookies>')
 def catch_all(cookies):
     return 'Sorry! No response here. Try url again.'
+
 # below is flask boiler plate; exclude it and stuff won't work    
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
